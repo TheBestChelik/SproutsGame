7bf0478ade0ac7e11d2c8d16f70f0025f2b5c3a6
Oleksandr <vasa22292@gmail.com>
2023-12-03 21:17:00 +0100

    fix(modules): merge Delaunay and Voronoi into geometry module

--- SproutGame/modules/geometry.py
+++ SproutGame/modules/geometry.py
@@ -1,7 +1,41 @@
-import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import numpy as np
 import itertools
+from typing import List, Tuple
+
+from SproutGame.primitives import Vertex
+from SproutGame.modules.Delaunator import Delaunator
+
+
+class Delaunay:
+    def __init__(self, vertices: List[Vertex]) -> None:
+        self.vertices = vertices
+
+    @property
+    def edges(self) -> List[Tuple[Vertex, Vertex]]:
+        start = 0
+        end = 3
+        count = 0
+        triangles = Delaunator(self.vertices).triangles
+        triangles_list = list(range(len(triangles)//3))
+        for idx in triangles_list:
+            triangles_list[count] = tuple(triangles[start:end])
+            start += 3
+            end += 3
+            count += 1
+        all_edges_list = []
+        for i in range(len(triangles_list)):
+            for idx, a in enumerate(triangles_list[i]):
+                for b in triangles_list[i][idx + 1:]:
+                    all_edges_list.append(tuple({a, b}))
+
+        edges_list = list(set(all_edges_list))
+        # v1, v2 are indexes in vertices list
+        edges = []
+        for v1, v2 in edges_list:
+            edges.append((self.vertices[v1], self.vertices[v2]))
+        return edges
+    
 
 
 class Voronoi:
@@ -100,4 +134,3 @@ class Voronoi:
     def ridge_vertices(self):
         ridge_vertices = self.ridge_vertices_list
         return ridge_vertices
-        
